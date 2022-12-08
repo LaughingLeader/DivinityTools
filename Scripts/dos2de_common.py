bb43e539e5b7508d648ec14ef3a5c45ef2fa33f1
LaughingLeader <laughingleader@gmail.com>
2022-12-08 06:27:04 -0600

    Rework dos2de_create_atlas to use argparse and texconv

--- Scripts/dos2de_common.py
+++ Scripts/dos2de_common.py
@@ -165,17 +165,20 @@ def trim(text:str):
     return "\n".join([s for s in text.splitlines() if s.strip()])
 
 def clear_log(file_name: str):
-  log_path = Path(__file__).parent.joinpath(f"_Logs/{file_name}").with_suffix(".log")
-  with log_path.open('w', encoding='utf-8') as f:
-    f.write("")
+    log_path = Path(__file__).parent.joinpath(f"_Logs/{file_name}").with_suffix(".log")
+    with log_path.open('w', encoding='utf-8') as f:
+        f.write("")
   
-def log(file_name: str, msg: str):
-  print(msg)
-  log_path = Path(__file__).parent.joinpath(f"_Logs/{file_name}").with_suffix(".log")
-  log_path.parent.mkdir(exist_ok=True, parents=True)
-  with log_path.open('a+', encoding='utf-8') as f:
-    f.seek(0)
-    data = f.read(100)
-    if len(data) > 0:
-      f.write("\n")
-    f.write(msg)
+def log(file_name: str, msg: str, do_print:bool=True):
+    if msg is None or msg == "":
+        return
+    if do_print:
+        print(msg)
+    log_path = Path(__file__).parent.joinpath(f"_Logs/{file_name}").with_suffix(".log")
+    log_path.parent.mkdir(exist_ok=True, parents=True)
+    with log_path.open('a+', encoding='utf-8') as f:
+        f.seek(0)
+        data = f.read(100)
+        if len(data) > 0:
+            f.write("\n")
+            f.write(msg)
--- Scripts/dos2de_create_atlas.py
+++ Scripts/dos2de_create_atlas.py
@@ -1,18 +1,18 @@
 from PIL import Image
 import os
-import sys
-import uuid
 import subprocess
 import pathlib
 from pathlib import Path
 import numpy
+import argparse
+import timeit
+from dataclasses import dataclass
+import send2trash
 
 import dos2de_common as common
+
 script_name = Path(__file__).stem
 common.clear_log(script_name)
-
-import time
-start_time = time.time()
 
 atlas_template = """<?xml version="1.0" encoding="UTF-8" ?>
 <save>
@@ -45,20 +45,26 @@ atlas_template = """<?xml version="1.0" encoding="UTF-8" ?>
 </save>
 """
 
-class Rect():
-    def __init__(self, x, y, w, h):
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
+@dataclass
+class UV():
+    u1:float
+    v1:float
+    u2:float
+    v2:float
 
+@dataclass
 class Icon():
-    def __init__(self, image_path, x,y,u1,v1,u2,v2):
-        self.image_path = image_path
-        self.image = Image.open(image_path).convert("RGBA")
-        self.name = Path(image_path).stem
-        self.pos = (x,y)
-        self.uv = Rect(u1,v1,u2,v2)
+    image_path:Path
+    pos:tuple[float,float]
+    uv:UV
+    image:Image.Image = None
+    
+    @property
+    def name(self):
+        return self.image_path.stem
+    
+    def __post_init__(self):
+        self.image = Image.open(self.image_path).convert("RGBA")
     
     def to_xml(self):
         return """
@@ -70,10 +76,10 @@ class Icon():
                     <attribute id="V2" value="{v2}" type="6" />
                 </node>""".format(
                     name = self.name,
-                    u1 = self.uv.x,
-                    v1 = self.uv.y,
-                    u2 = self.uv.w,
-                    v2 = self.uv.h
+                    u1 = self.uv.u1,
+                    v1 = self.uv.v1,
+                    u2 = self.uv.u2,
+                    v2 = self.uv.v2
                 )
 
 def get_images(directory):
@@ -81,154 +87,152 @@ def get_images(directory):
     files = dir_path.glob("*.png")
     return list(files)
 
-import math
 def truncate(number, digits, round_num = True) -> float:
     stepper = 10.0 ** digits
     if round_num:
-        return round(math.trunc(stepper * number) / stepper, digits - 1)
+        return round(numpy.trunc(stepper * number) / stepper, digits - 1)
     else:
-        return math.trunc(stepper * number) / stepper
-
-def get_arg(arg, fallback):
-    if len(sys.argv) > arg:
-        val = sys.argv[arg]
-        if val != None:
-            return val
-    return fallback
+        return numpy.trunc(stepper * number) / stepper
 
 script_dir = Path(os.path.dirname(os.path.abspath(__file__)))
 os.chdir(script_dir)
 
-images_dir = get_arg(1, script_dir.joinpath("icons"))
-atlas_output = get_arg(2, script_dir.joinpath("AllIcons_Merged.lsx"))
-texture_output = get_arg(3, script_dir.joinpath("AllIcons_Merged.png"))
-atlas_uuid = get_arg(4, str(uuid.uuid4()))
-texture_dds_output = get_arg(5, "")
-if texture_dds_output == "":
-    texture_dds_output = Path(texture_output).with_suffix(".dds")
-else:
-    # Ensure ext
-    texture_dds_output = Path(texture_dds_output).with_suffix(".dds")
+# python dos2de_create_atlas.py -i "D:\_Temp\Atlas\icons" -a "D:\_Temp\Atlas\MyAtlas.lsx" -t "D:\_Temp\Atlas\MyAtlas.dds"
+# python dos2de_create_atlas.py -i "D:\_Temp\Atlas\icons" -a "G:\Divinity Original Sin 2\DefEd\Data\Public\TestMod_Experiments_301b72cf-d231-4b3d-813b-115806f90146\GUI\MyAtlas.lsx" -t "G:\Divinity Original Sin 2\DefEd\Data\Public\TestMod_Experiments_301b72cf-d231-4b3d-813b-115806f90146\Assets\Textures\Icons\MyAtlas.dds"
 
-texture_name = Path(texture_output).stem
-texture_xml_output = "Assets/Textures/Icons/{}.dds".format(texture_name)
+parser = argparse.ArgumentParser(description='Create a texture atlas from a folder of icons.')
+parser.add_argument("-i", "--icons", type=Path, required=True, help='The directory of icons to process.')
+parser.add_argument("-a", "--atlas", type=Path, required=True, help='The path of the atlas file to create, such as Public/ModName_UUID/GUI/MyAtlas.lsx.')
+parser.add_argument("-t", "--texture", type=Path, required=True, help='The path of the texture to create.')
+parser.add_argument("-d", "--ddstool", type=Path, help='The path to the "DirectXTex texture processing library" (DirectXTex).')
+parser.add_argument("-u", "--uuid", type=str, default="", help='The UUID to use for the atlas (defaults to a new UUID4).')
+parser.add_argument("-m", "--mipmaps", action='store_true', help='Generate mipmaps (default False).')
+parser.add_argument("-f", "--ddsformat", type=str, default="DXT5", help='The dds format to use (DXT1, DXT5 etc). Defaults to DXT5 (BC3_UNORM).')
+parser.add_argument("--iconsize", type=int, default=(64,64), nargs="+", help='The icon width/height/.')
+parser.add_argument("--texturesize", type=int, default=(2048,2048), nargs="+", help='The texture width/height/.')
 
-atlas_size = 4096
-icon_size = 64
+totalIcons = 0
 
-images = get_images(images_dir)
-common.log(script_name, "Merging {} images.".format(len(images)))
+def run():
+    args = parser.parse_args()
+    images_dir:Path = args.icons
+    atlas_output:Path = args.atlas
+    texture_output:Path = args.texture
+    atlas_uuid:str = args.uuid
+    dds_format:str = args.ddsformat
+    icon_size:tuple[int,int] = args.iconsize
+    texture_size:tuple[int,int] = args.texturesize
+    
+    if atlas_uuid is None or atlas_uuid == "":
+        atlas_uuid = common.NewUUID()
+    do_mipmaps:bool = args.mipmaps
 
-icons = []
+    atlas_output = atlas_output.with_suffix(".lsx")
+    texture_output = texture_output.with_suffix(".dds")
+    temp_texture_png = texture_output.with_suffix(".png")
+    root_data_dir = atlas_output.parent.parent.parent
 
-padding = float(0.5/atlas_size)
-col_max = atlas_size / icon_size
-row_max = atlas_size / icon_size
+    images = get_images(images_dir)
+    common.log(script_name, f"Merging {len(images)} images.")
 
-x = 0
-y = 0
+    icons = []
 
-# For some reason, the editor appends the first two icons to the end
-icons_first = []
+    padding = (float(0.5/texture_size[0]), float(0.5/texture_size[1]))
+    col_max = texture_size[0] / icon_size[0]
+    row_max = texture_size[1] / icon_size[1]
 
-for img in images:
-    # u1 = truncate(float(((icon_size * x) / atlas_size) + padding), 9)
-    # v1 = truncate(float(((icon_size * y) / atlas_size) + padding), 9)
-    # u2 = truncate(float(((icon_size * (x + 1)) / atlas_size) - padding), 9)
-    # v2 = truncate(float(((icon_size * (y + 1)) / atlas_size) - padding), 9)
-    round_num = True
-    truncate_u1 = 7
-    truncate_v1 = 8
-    truncate_u2 = 7
-    truncate_v2 = 7
+    x = 0
+    y = 0
 
-    if x <= 1 and y == 0: 
-        u1 = truncate(numpy.clip(float(((icon_size * x) / atlas_size) + padding), 0, 1.0), 9, round_num=True)
-        v1 = truncate(numpy.clip(float(((icon_size * y) / atlas_size) + padding), 0, 1.0), 9, round_num=False)
-        u2 = truncate(numpy.clip(float(((icon_size * (x + 1)) / atlas_size) - padding), 0, 1.0), 7, round_num=False)
-        v2 = truncate(numpy.clip(float(((icon_size * (y + 1)) / atlas_size) - padding), 0, 1.0), 7, round_num=False)
-    else:
-        u1 = truncate(numpy.clip(float(((icon_size * x) / atlas_size) + padding), 0, 1.0), truncate_u1, round_num)
-        v1 = truncate(numpy.clip(float(((icon_size * y) / atlas_size) + padding), 0, 1.0), truncate_v1, round_num)
-        u2 = truncate(numpy.clip(float(((icon_size * (x + 1)) / atlas_size) - padding), 0, 1.0), truncate_u2, round_num)
-        v2 = truncate(numpy.clip(float(((icon_size * (y + 1)) / atlas_size) - padding), 0, 1.0), truncate_v2, round_num)
+    # For some reason, the editor appends the first two icons to the end
+    icons_first = []
+    for img in images:
+        # u1 = truncate(float(((icon_size * x) / texture_size) + padding), 9)
+        # v1 = truncate(float(((icon_size * y) / texture_size) + padding), 9)
+        # u2 = truncate(float(((icon_size * (x + 1)) / texture_size) - padding), 9)
+        # v2 = truncate(float(((icon_size * (y + 1)) / texture_size) - padding), 9)
+        round_num = True
+        truncate_u1 = 7
+        truncate_v1 = 8
+        truncate_u2 = 7
+        truncate_v2 = 7
 
-    icon = Icon(
-        img.resolve(),
-        x * icon_size, #math.floor(atlas_size * u1),
-        y * icon_size, #math.floor(atlas_size * v1),
-        u1,v1,u2,v2)
+        if x <= 1 and y == 0: 
+            u1 = truncate(numpy.clip(float(((icon_size[0] * x) / texture_size[0]) + padding[0]), 0, 1.0), 9, round_num=True)
+            v1 = truncate(numpy.clip(float(((icon_size[1] * y) / texture_size[0]) + padding[0]), 0, 1.0), 9, round_num=False)
+            u2 = truncate(numpy.clip(float(((icon_size[0] * (x + 1)) / texture_size[1]) - padding[1]), 0, 1.0), 7, round_num=False)
+            v2 = truncate(numpy.clip(float(((icon_size[1] * (y + 1)) / texture_size[1]) - padding[1]), 0, 1.0), 7, round_num=False)
+        else:
+            u1 = truncate(numpy.clip(float(((icon_size[0] * x) / texture_size[0]) + padding[0]), 0, 1.0), truncate_u1, round_num)
+            v1 = truncate(numpy.clip(float(((icon_size[1] * y) / texture_size[0]) + padding[0]), 0, 1.0), truncate_v1, round_num)
+            u2 = truncate(numpy.clip(float(((icon_size[0] * (x + 1)) / texture_size[1]) - padding[1]), 0, 1.0), truncate_u2, round_num)
+            v2 = truncate(numpy.clip(float(((icon_size[1] * (y + 1)) / texture_size[1]) - padding[1]), 0, 1.0), truncate_v2, round_num)
 
-    if x <= 1 and y == 0:
-        icons_first.append(icon)
-    else:
-        icons.append(icon)
-    common.log(script_name, "** Added icon '{}'.".format(icon.name))
-    x += 1
-    if(x >= col_max):
-        y += 1
-        x = 0
-    if (y > row_max):
-        common.log(script_name, "[ERROR] Hit the max atlas size!")
-        break
+        icon = Icon(img.resolve(), (x * icon_size[0], y * icon_size[1]), UV(u1, v1, u2, v2))
 
-icons.extend(icons_first)
-total = len(icons)
+        if x <= 1 and y == 0:
+            icons_first.append(icon)
+        else:
+            icons.append(icon)
+        common.log(script_name, f"** Added icon '{icon.name}'.", False)
+        x += 1
+        if(x >= col_max):
+            y += 1
+            x = 0
+        if (y > row_max):
+            common.log(script_name, "[ERROR] Hit the max atlas size!")
+            break
 
-def create_atlas_output(icons_str, icon_w, icon_h, 
-        texture_path, uuid, texture_width, texture_height):
-    return atlas_template.format(
-        icons = icons_str,
-        icon_w = icon_w,
-        icon_h = icon_h,
-        texture_path = texture_path,
-        texture_uuid = uuid,
-        texture_width = texture_width,
-        texture_height = texture_height
-    )
+    icons.extend(icons_first)
+    global totalIcons
+    totalIcons = len(icons)
 
-icons_str = ""
-for icon in icons:
-    icons_str += icon.to_xml()
+    def create_atlas_output(icons_str, icon_w, icon_h, 
+            texture_path, uuid, texture_width, texture_height):
+        return atlas_template.format(
+            icons = icons_str,
+            icon_w = icon_w,
+            icon_h = icon_h,
+            texture_path = texture_path,
+            texture_uuid = uuid,
+            texture_width = texture_width,
+            texture_height = texture_height
+        )
 
-xml_str = create_atlas_output(icons_str, icon_size, icon_size, 
-    texture_xml_output, atlas_uuid, atlas_size, atlas_size)
+    icons_str = ""
+    for icon in icons:
+        icons_str += icon.to_xml()
 
-common.log(script_name, "Saving atlas to '{}'.".format(atlas_output))
+    xml_str = create_atlas_output(icons_str, icon_size[0], icon_size[1], 
+        texture_output.relative_to(root_data_dir), atlas_uuid, texture_size[0], texture_size[1])
 
-f = open(atlas_output, "w")
-f.write(xml_str)
+    common.log(script_name, f"Saving atlas to '{atlas_output}'.")
 
-texture_image = Image.new('RGBA', (atlas_size, atlas_size), (0, 0, 0, 0))
+    f = open(atlas_output, "w")
+    f.write(xml_str)
 
-for icon in icons:
-    common.log(script_name, "** Pasting '{}'.".format(icon.name))
-    #texture_image.paste(icon.image, icon.pos, mask=0)
-    texture_image.alpha_composite(icon.image, icon.pos)
+    texture_image = Image.new('RGBA', texture_size, (0, 0, 0, 0))
 
-common.log(script_name, "Saving texture to '{}'.".format(texture_output))
+    for icon in icons:
+        common.log(script_name, f"** Pasting '{icon.name}'.", False)
+        #texture_image.paste(icon.image, icon.pos, mask=0)
+        texture_image.alpha_composite(icon.image, icon.pos)
 
-texture_image.save(texture_output)
+    common.log(script_name, f"Saving temp png texture to '{temp_texture_png}'.")
+    texture_image.save(temp_texture_png)
+    common.log(script_name, f"Converting texture to dds at '{texture_output}'.")
 
-common.log(script_name, "Done. Took **{} seconds** to merge {} icons.".format(time.time() - start_time, total))
-
-#convert_params = 'nvcompress -alpha -bc3 "{}" "{}"'.format(texture_output, texture_dds_output)
-
-# p = subprocess.run([convert_params],
-#   shell=True,
-#   universal_newlines=True, 
-#   stdout=subprocess.PIPE, 
-#   stderr=subprocess.PIPE)
-
-p = subprocess.run(["nvcompress", 
-    "-alpha", 
-    "-bc3", 
-    str(texture_output), 
-    str(texture_dds_output),
-    "-srgb"
-    ], 
-    universal_newlines=True, 
-    stdout=subprocess.PIPE, 
-    stderr=subprocess.PIPE)
-common.log(script_name, p.stdout)
-common.log(script_name, p.stderr)
+    command = f"texconv -m {0 if do_mipmaps == True else 1} -ft dds -f {dds_format} -nologo -timing -y -o \"{texture_output.parent}\" \"{temp_texture_png.absolute()}\""
+    common.log(script_name, f"Running command '{command}'.")
+    p = subprocess.run(command,
+        shell=True,
+        universal_newlines=True,
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE)
+    #common.log(script_name, f"{p.args}")
+    common.log(script_name, p.stdout)
+    common.log(script_name, p.stderr)
+    if p.returncode == 0:
+        send2trash.send2trash(str(temp_texture_png.absolute()).replace("/", "\\"))
+        return True
+print("Created atlas in {} seconds for {} icons.".format(timeit.timeit(run, number=1), totalIcons))
